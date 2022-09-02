3b654b0df25a4c74311d2545cddf1803c75065a6
Saiful-Islam <saiful.islam.rafi.88@gmail.com>
2022-09-02 19:51:51 +0600

    add more js function

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,31 @@
 // For top section
 const listOpts = document.querySelectorAll('.col-1 .tab-title ul li')
 const contants = document.querySelectorAll('.col-1 .tab-content .info')
-var menu = document.getElementById('preview2')
+const thumbnails = document.querySelectorAll('.col-2 .thumbnail')
+const foodmenus = document.querySelectorAll('.section')
 
 listOpts.forEach((opt, idx) => {
   opt.addEventListener('click', () => {
     hideAllContants()
     hideAllOpts()
+    hideAllThumbnails()
+    hideAllFoodmenus()
 
     opt.classList.add('on')
     contants[idx].classList.add('visible')
+    thumbnails[idx].classList.add('open')
+    foodmenus[idx].classList.add('watch')
   })
 })
 
 function hideAllContants() {
   contants.forEach(contant => contant.classList.remove('visible'))
+}
+function hideAllFoodmenus() {
+  foodmenus.forEach(foodmenu => foodmenu.classList.remove('watch'))
+}
+function hideAllThumbnails() {
+  thumbnails.forEach(thumbnail => thumbnail.classList.remove('open'))
 }
 function hideAllOpts() {
   listOpts.forEach(opt => opt.classList.remove('on'))
